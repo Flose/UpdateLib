691e059953e94654c5d197e12e92493acb21c586
Flose <flose@mal-was-anderes.de>
2007-10-03 19:34:51 +0000

    Version 1.2 Update alle lizenzen anzeigen (nicht nur in update verzeichnis, sonder auch im hauptverzeichnis);

--- frmLizenz.vb
+++ frmLizenz.vb
@@ -2,9 +2,18 @@ Public Class frmLizenz
     Private Sub frmLizenz_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         lblReleasenotes.Text = System.String.Format(lblReleasenotes.Text, System.Environment.GetCommandLineArgs(1))
         Dim tmpDatei As String
+        'Lizenzen aus Update verzeichnis(haben vorrang)
         For Each Datei As String In System.IO.Directory.GetFiles(Application.StartupPath & "\Update\", "Lizenz-*.txt")
             tmpDatei = System.IO.Path.GetFileNameWithoutExtension(Datei)
             cmbSprachen.Items.Add(tmpDatei.Substring(7))
+        Next
+
+        'Lizenzen aus hauptverzeichnis
+        For Each Datei As String In System.IO.Directory.GetFiles(Application.StartupPath & "\", "Lizenz-*.txt")
+            tmpDatei = System.IO.Path.GetFileNameWithoutExtension(Datei)
+            If cmbSprachen.Items.IndexOf(tmpDatei) = -1 Then '=>Datei nicht in update verzeichnis
+                cmbSprachen.Items.Add(tmpDatei.Substring(7))
+            End If
         Next
         cmbSprachen.SelectedIndex = 0
     End Sub
@@ -12,9 +21,17 @@ Public Class frmLizenz
     Private Sub cmbSprachen_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbSprachen.SelectedIndexChanged
         If cmbSprachen.SelectedIndex > -1 Then
             Try
-                Dim Reader As New System.IO.StreamReader(Application.StartupPath & "\Update\Lizenz-" & CStr(cmbSprachen.SelectedItem) & ".txt", True)
-                txtLizenz.Text = Reader.ReadToEnd
-                Reader.Close()
+                If System.IO.File.Exists(Application.StartupPath & "\Update\Lizenz-" & CStr(cmbSprachen.SelectedItem) & ".txt") Then
+                    Dim Reader As New System.IO.StreamReader(Application.StartupPath & "\Update\Lizenz-" & CStr(cmbSprachen.SelectedItem) & ".txt", True)
+                    txtLizenz.Text = Reader.ReadToEnd
+                    Reader.Close()
+                ElseIf System.IO.File.Exists(Application.StartupPath & "\Lizenz-" & CStr(cmbSprachen.SelectedItem) & ".txt") Then
+                    Dim Reader As New System.IO.StreamReader(Application.StartupPath & "\Lizenz-" & CStr(cmbSprachen.SelectedItem) & ".txt", True)
+                    txtLizenz.Text = Reader.ReadToEnd
+                    Reader.Close()
+                Else
+                    txtLizenz.Text = ""
+                End If
             Catch ex As Exception
                 txtLizenz.Text = ""
             End Try
