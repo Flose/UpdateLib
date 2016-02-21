79515351dc74cbdceeec0cc5c25040aa288b1091
Sebastian <asamk@gmx.de>
2016-02-21 23:10:51 +0100

    Add Update.exe as contentFile to nupkg

--- make_package.bat
+++ make_package.bat
@@ -1,10 +1,11 @@
 REM Build Solution
-set PATH_SOURCE_PROJ="%cd%\UpdateLib.vbproj"
+set PATH_SOURCE_SLN="%cd%\UpdateLib.sln"
+set PATH_SOURCE_PROJ="%cd%\UpdateLib\UpdateLib.vbproj"
 
-nuget restore %PATH_SOURCE_PROJ%
+nuget restore %PATH_SOURCE_SLN%
 
 FOR %%C IN (Release20 Release40) DO (
- "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_PROJ% /p:Configuration=%%C
+ "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_SLN% /p:Configuration=%%C
  if errorlevel 1 goto :eof
 )
 
