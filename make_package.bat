961056ee8ee3a550eabff3074257ebe82e96efa1
Sebastian <asamk@gmx.de>
2016-03-06 14:04:09 +0100

    Complete rebuild when making nuget package

--- make_package.bat
+++ make_package.bat
@@ -5,7 +5,7 @@ set PATH_SOURCE_PROJ="%cd%\UpdateLib\UpdateLib.vbproj"
 nuget restore %PATH_SOURCE_SLN%
 
 FOR %%C IN (Release20 Release40) DO (
- "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_SLN% /p:Configuration=%%C
+ "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_SLN% /t:Rebuild /p:Configuration=%%C
  if errorlevel 1 goto :eof
 )
 
