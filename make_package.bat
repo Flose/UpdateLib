6ae8532a675ff23987bc3baa820fabfb6803c992
Sebastian <asamk@gmx.de>
2019-01-27 11:14:06 +0100

    Convert package.json to <PackageReference>s

--- make_package.bat
+++ make_package.bat
@@ -1,13 +1,14 @@
 REM Build Solution
 set PATH_SOURCE_SLN="%cd%\UpdateLib.sln"
 set PATH_SOURCE_PROJ="%cd%\UpdateLib\UpdateLib.vbproj"
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\bin\MSBuild.exe"
 
-nuget.exe update -self
-nuget restore %PATH_SOURCE_SLN%
 
 FOR %%C IN (Release20 Release40) DO (
- "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %PATH_SOURCE_SLN% /t:Rebuild /p:Configuration=%%C
+ %MSBUILD% %PATH_SOURCE_SLN% /t:Restore /p:Configuration=%%C
+ %MSBUILD% %PATH_SOURCE_SLN% /t:Rebuild /p:Configuration=%%C
  if errorlevel 1 goto :eof
 )
 
+nuget.exe update -self
 nuget pack %PATH_SOURCE_PROJ% -Prop Configuration=Release20 -Symbols -IncludeReferencedProjects -Verbosity detailed
